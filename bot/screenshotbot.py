518a2af30c5dbdb5dd792e651b088bcd5381254f
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-04-30 10:57:30 +0530

    Update screenshotbot.py

--- bot/screenshotbot.py
+++ bot/screenshotbot.py
@@ -8,14 +8,13 @@ class ScreenShotBot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name=Config.SESSION_NAME,
-            bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="bot/plugins"
-            )
+    Config.SESSION_NAME,
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    workers=20,
+    plugins=dict(root="bot/plugins")
+        )
         )
 
         self.db = Database(Config.DATABASE_URL, Config.SESSION_NAME)
